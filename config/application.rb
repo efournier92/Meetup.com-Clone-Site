775d9fba07a0657169d946aadd196eb2d821ca31
Misigno <efournier92@gmail.com>
2016-05-19 22:48:10 -0400

    Fix config/application.rb OmniAuth issue

--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,23 @@
 configure :development, :test do
-  require 'dotenv'
-  Dotenv.load
-  require 'pry'
+	require 'dotenv'
+	Dotenv.load
+	require 'pry'
 end
 
 configure do
-  require 'sinatra/activerecord'
-  require 'sinatra/flash'
-  require 'omniauth-github'
+	require 'sinatra/activerecord'
+	require 'sinatra/flash'
+	require 'omniauth-github'
 
-  Dir[__dir__ + "/../app/**/*.rb"].each do |file|
-    require_relative file
-  end
+	Dir[__dir__ + "/../app/**/*.rb"].each do |file|
+		require_relative file
+	end
 
-  enable :sessions
-  set :session_secret, ENV['SESSION_SECRET']
+	enable :sessions
+	set :session_secret, ENV['SESSION_SECRET']
 
-  set :views, 'app/views'
-
-    provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user:email'
-  end
+	set :views, 'app/views'
+	use OmniAuth::Builder do
+		provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user:email'
+	end
 end
